6e8bdd782e43604aba05b283847c5a0144cc0d71
cruddasj <james.cruddas@informed.com>
2019-11-12 13:32:44 +0000

    CPD and package name fix

--- src/main/java/uk/gov/caz/retrofit/amazonaws/StreamLambdaHandler.java
+++ src/main/java/uk/gov/caz/retrofit/amazonaws/StreamLambdaHandler.java
@@ -1,4 +1,4 @@
-package uk.gov.caz.vcc.amazonaws;
+package uk.gov.caz.retrofit.amazonaws;
 
 import static uk.gov.caz.awslambda.AwsHelpers.splitToArray;
 
@@ -10,9 +10,7 @@ import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import uk.gov.caz.retrofit.Application;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,12 +20,12 @@ import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
-import java.util.UUID;
+
 import org.springframework.util.StreamUtils;
+
+import uk.gov.caz.retrofit.Application;
+import uk.gov.caz.retrofit.dto.LambdaContainerStats;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
 
@@ -115,50 +113,5 @@ public class StreamLambdaHandler implements RequestStreamHandler {
    */
   private boolean isWarmupRequest(String action) {
     return action.contains(KEEP_WARM_ACTION);
-  }
-
-  static class LambdaContainerStats {
-    private static final String INSTANCE_ID = UUID.randomUUID().toString();
-    private static final DateTimeFormatter formatter =
-        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-    private static LocalDateTime latestRequestTime;
-
-    private LambdaContainerStats() {
-      throw new IllegalStateException("Utility class");
-    }
-
-    /**
-     * Set the time that the container serves a request.
-     */
-    public static void setLatestRequestTime(LocalDateTime dt) {
-      latestRequestTime = dt;
-    }
-
-    /**
-     * Get the time that the container last served a request.
-     */
-    public static LocalDateTime getLatestRequestTime() {
-      return latestRequestTime;
-    }
-
-    /**
-     * Get the container stats.
-     *
-     * @return a string that contains lambda container Id and (optionally) the time
-     *         that the container last serve a request.
-     */
-    public static String getStats() {
-      try {
-        ObjectMapper obj = new ObjectMapper();
-        Map<String, String> retVal = new HashMap<>();
-        retVal.put("instanceId", INSTANCE_ID);
-        if (latestRequestTime != null) {
-          retVal.put("latestRequestTime",latestRequestTime.format(formatter));
-        }
-        return obj.writeValueAsString(retVal);
-      } catch (JsonProcessingException ex) {
-        return String.format("\"instanceId\": \"%s\"", INSTANCE_ID);
-      }
-    }
   }
 }
