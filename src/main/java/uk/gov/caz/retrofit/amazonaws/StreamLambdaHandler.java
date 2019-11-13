bf8ecd7375e153dfaa8865e5146bc144895ce935
cruddasj <james.cruddas@informed.com>
2019-11-13 19:03:53 +0000

    Update to sync startup handler

--- src/main/java/uk/gov/caz/retrofit/amazonaws/StreamLambdaHandler.java
+++ src/main/java/uk/gov/caz/retrofit/amazonaws/StreamLambdaHandler.java
@@ -1,86 +1,114 @@
 package uk.gov.caz.retrofit.amazonaws;
 
-import static uk.gov.caz.awslambda.AwsHelpers.splitToArray;
-
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.internal.LambdaContainerHandler;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
-import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Splitter;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.nio.charset.Charset;
-import java.time.Instant;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
-
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
-
+import org.jetbrains.annotations.NotNull;
 import org.springframework.util.StreamUtils;
-
 import uk.gov.caz.retrofit.Application;
-import uk.gov.caz.retrofit.dto.LambdaContainerStats;
 
 @Slf4j
 public class StreamLambdaHandler implements RequestStreamHandler {
 
   private static final String KEEP_WARM_ACTION = "keep-warm";
+  private static final Splitter SPLITTER = Splitter.on(',')
+      .trimResults()
+      .omitEmptyStrings();
   
-  /*
-   * This field is `static` to avoid being garbage collected and in turn it prevents the application
-   * from being initialized more than once within one Lambda deployment
-   */
-  private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+  private SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
-  static {
-    long startTime = Instant.now().toEpochMilli();
-    try {
-      // For applications that take longer than 10 seconds to start, use the async builder:
-      String listOfActiveSpringProfiles = System.getenv("SPRING_PROFILES_ACTIVE");
-      LambdaContainerHandler.getContainerConfig().setInitializationTimeout(50_000);
-      if (listOfActiveSpringProfiles != null) {
-        handler = new SpringBootProxyHandlerBuilder()
-            .defaultProxy()
-            .asyncInit(startTime)
-            .springBootApplication(Application.class)
-            .profiles(splitToArray(listOfActiveSpringProfiles))
-            .buildAndInitialize();
-      } else {
-        handler = new SpringBootProxyHandlerBuilder()
-            .defaultProxy()
-            .asyncInit(startTime)
-            .springBootApplication(Application.class)
-            .buildAndInitialize();
+  /**
+   * Private method for instantiating a Spring handler to serve the inbound
+   * request.
+   */
+  private void initialiseSpringHandler() {
+    if (handler == null) {
+      try {
+        String listOfActiveSpringProfiles = System.getenv("SPRING_PROFILES_ACTIVE");
+        if (listOfActiveSpringProfiles != null) {
+          handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class,
+              splitToArray(listOfActiveSpringProfiles));
+        } else {
+          handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+        }
+      } catch (ContainerInitializationException e) {
+        // if we fail here. We re-throw the exception to force another cold
+        // start
+        log.error(e.getMessage());
+        throw new RuntimeException(
+            "Could not initialize Spring Boot application", e);
       }
-    } catch (ContainerInitializationException e) {
-      // if we fail here. We re-throw the exception to force another cold start
-      throw new RuntimeException("Could not initialize Spring Boot application", e);
     }
   }
-
+  
+  /**
+   * Private helper for splitting a comma delimited string to an array.
+   */
+  private String[] splitToArray(String activeProfiles) {
+    return SPLITTER.splitToList(activeProfiles).toArray(new String[0]);
+  }
+  
   @Override
   public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
       throws IOException {
+    
+    // Initialise Spring handler under both real request conditions
+    // as well as warmup requests.
+    initialiseSpringHandler();
 
-    String input = StreamUtils.copyToString(inputStream, Charset.defaultCharset());    
-    log.info("Input received: " + input);
-    if (isWarmupRequest(input)) {
+    byte[] inputBytes = StreamUtils.copyToByteArray(inputStream);
+
+    if (isWarmupRequest(toString(inputBytes))) {
       delayToAllowAnotherLambdaInstanceWarming();
       try (Writer osw = new OutputStreamWriter(outputStream)) {
         osw.write(LambdaContainerStats.getStats());
       }
     } else {
+      log.info("Processing API request");
       LambdaContainerStats.setLatestRequestTime(LocalDateTime.now());
-      handler.proxyStream(new ByteArrayInputStream(input.getBytes()), outputStream, context);
+      handler.proxyStream(toInputStream(inputBytes), outputStream, context);
     }
+  }
+
+  /**
+   * Converts byte array to {@link InputStream}.
+   *
+   * @param inputBytes Input byte array.
+   * @return {@link InputStream} over byte array.
+   * @throws IOException When unable to convert.
+   */
+  @NotNull
+  private InputStream toInputStream(byte[] inputBytes) throws IOException {
+    try (InputStream inputStream = new ByteArrayInputStream(inputBytes)) {
+      return inputStream;
+    }
+  }
+
+  /**
+   * Converts {@code inputBytes} to an UTF-8 encoded string.
+   */
+  private String toString(byte[] inputBytes) {
+    return new String(inputBytes, StandardCharsets.UTF_8);
   }
 
   /**
@@ -112,7 +140,56 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     if (isWarmupRequest) {
       log.info("Received lambda warmup request");
     }
-    
+
     return isWarmupRequest;
+  }
+
+  /**
+   * Contain information about the lambda container.
+   */
+  static class LambdaContainerStats {
+
+    private static final String INSTANCE_ID = UUID.randomUUID().toString();
+    private static final DateTimeFormatter formatter =
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    private static LocalDateTime latestRequestTime;
+
+    private LambdaContainerStats() {
+      throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * Set the time that the container serves a request.
+     */
+    public static void setLatestRequestTime(LocalDateTime dt) {
+      latestRequestTime = dt;
+    }
+
+    /**
+     * Get the time that the container last served a request.
+     */
+    public static LocalDateTime getLatestRequestTime() {
+      return latestRequestTime;
+    }
+
+    /**
+     * Get the container stats.
+     *
+     * @return a string that contains lambda container Id and (optionally) the time that the
+     *     container last serve a request.
+     */
+    public static String getStats() {
+      try {
+        ObjectMapper obj = new ObjectMapper();
+        Map<String, String> retVal = new HashMap<>();
+        retVal.put("instanceId", INSTANCE_ID);
+        if (latestRequestTime != null) {
+          retVal.put("latestRequestTime", latestRequestTime.format(formatter));
+        }
+        return obj.writeValueAsString(retVal);
+      } catch (JsonProcessingException ex) {
+        return String.format("\"instanceId\": \"%s\"", INSTANCE_ID);
+      }
+    }
   }
 }
